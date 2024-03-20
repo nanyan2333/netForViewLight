755db87ae459f340b29832e5accf92e8b686ed8b
AivyChen <145818007+AivyChen@users.noreply.github.com>
2024-03-20 22:06:53 +0800

    Add files via upload

---
+++ src/randomBin/softdecode/decode/check.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+void checkVout(char* in_bin_path, char* out_bin_path, char* vout_bin_path);
