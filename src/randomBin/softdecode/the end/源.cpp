82755f4905e8bf75d4a7d531f692bb5091f4ceb4
Lee Mark <117705652+MRORANGE11112@users.noreply.github.com>
2024-03-22 11:05:30 +0800

    Update 源.cpp

--- src/randomBin/softdecode/the end/源.cpp
+++ src/randomBin/softdecode/the end/源.cpp
@@ -1,56 +1,169 @@
-#include "parsingQRcodes.h"
+#include "identifyQRcode.h"
 
-//接收图像作为参数，返回图像最小矩形框
-cv::Rect getMinRectBound(const cv::Mat& img) {
-    CV_Assert(img.type() == CV_8UC1);  // 确保图像是单通道灰度图
-
-    bool key = false;
-    int rows = img.rows, cols = img.cols;
-    cv::Point topRight(0, rows);//最上边、最右边
-    cv::Point bottomLeft(cols, 0);//最左边、最下边
-
-    for (int i = 0; i < rows; ++i) {
-        const uchar* row = img.ptr<uchar>(i);
-        for (int j = 0; j < cols; ++j) {
-            if (row[j] == 255) {  // 黑色背景，白色内容
-                topRight.x = std::max(topRight.x, j);
-                topRight.y = std::min(topRight.y, i);
-                bottomLeft.x = std::min(bottomLeft.x, j);
-                bottomLeft.y = std::max(bottomLeft.y, i);
-            }
-        }
-    }
-    return cv::Rect(bottomLeft.x, topRight.y, topRight.x - bottomLeft.x, bottomLeft.y - topRight.y);
+float GetAngle(Point2f v1, Point2f v2) {
+	float mod1 = sqrt(v1.x * v1.x + v1.y * v1.y);
+	float mod2 = sqrt(v2.x * v2.x + v2.y * v2.y);
+	float angle = acos((v1.x * v2.x + v1.y * v2.y) / (mod1 * mod2)) * 180.0 / CV_PI;
+	return angle;
 }
-int num = 0;
 
-void parsingQRcodes(Mat img, vector<Mat>& QRcodes) {
-    cv::Mat gray;
-    //灰度化、二值化：
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    for (int i = 0; i < 3; i++) {
-        //高斯模糊,循环增强平滑效果
-        cv::GaussianBlur(gray, gray, Size(3, 3), 0);
-    }
-    threshold(gray, gray, 127, 255, THRESH_BINARY);
+void IdentifyQRcode(Mat img, vector<Mat>& QRcodes) {
+	Mat midImg;
+	cvtColor(img, midImg, COLOR_BGR2GRAY);
 
-    // 获取最小矩形边框
-    cv::Rect rect = getMinRectBound(gray);
+	for (int i = 0; i < 3; i++) {
+		GaussianBlur(midImg, midImg, Size(3, 3), 0);
+	}
+	threshold(midImg, midImg, 127, 255, THRESH_BINARY);
+	//imwrite("./pic/" + to_string(QRcodes.size() - 1) + ".jpg", midImg);
 
-    // 创建一个新的空白画布，大小可以自定义  
-    cv::Mat qrcode = cv::Mat::zeros(cv::Size(rect.width, rect.height), CV_8UC1); // 创建一个780x780大小的空白画布，1通道（灰度图）  
+	//查找轮廓之间的层级关系
+	vector<vector<Point>>contours;
+	vector<Vec4i>hierarchy;
+	findContours(midImg, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
 
-    // 设置截取区域在新画布上的位置 
-    cv::Rect roiInqrcode(0, 0, rect.width, rect.height);
+	//父轮廓索引
+	int ParentIndex = -1;
+	int mark = 0;
 
-    // 将原图的截取区域复制到新画布上  
-    gray(rect).copyTo(qrcode(roiInqrcode));
+	vector<Point2f>rect_points;
+	double minRect = -1;
+	int minRectIdx = -1;
+	//Mat canva = Mat::zeros(midImg.size(), midImg.type());
+	for (int j = 0; j < contours.size(); j++) {
+		if (contourArea(contours[j]) > 50000) {
+			continue;
+		}
+		if (hierarchy[j][2] != -1 && mark == 0) {
+			ParentIndex = j;
+			mark++;
+		}
+		else if (hierarchy[j][2] != -1 && mark == 1) {
+			mark++;
+		}
+		else if (hierarchy[j][2] == -1) {
+			ParentIndex = -1;
+			mark = 0;
+		}
 
-    threshold(qrcode, qrcode, 127, 255, cv::THRESH_BINARY_INV);
-    string filename = to_string(num++) + ".png";
-    cv::imwrite(filename, qrcode);
+		if (hierarchy[j][2] != -1 && mark == 2) {
+			RotatedRect rect = minAreaRect(contours[ParentIndex]);
+			rect_points.push_back(rect.center);
 
-    // 保存截取的区域
-    QRcodes.push_back(qrcode);
+			if (minRect == -1) {
+				minRect = contourArea(contours[ParentIndex]);
+				minRectIdx = rect_points.size() - 1;
+			}
+			else {
+				if (contourArea(contours[ParentIndex]) < minRect) {
+					minRect = contourArea(contours[ParentIndex]);
+					minRectIdx = rect_points.size() - 1;
+				}
+			}
 
+			ParentIndex = -1;
+			mark = 0;
+			//drawContours(canva, contours, ParentIndex, Scalar::all(255), -1);
+			//imwrite("canva_1.png", canva);
+		}
+	}
+	//cout << minRectIdx << "\t" << rect_points.size() << endl;
+	if (rect_points.size() < 4) {	//因为帧数足够多，可以舍弃一些识别模糊的帧
+		return;
+	}
+
+	Point2f P1[4];
+	P1[3] = Point2f(rect_points[minRectIdx].x, rect_points[minRectIdx].y);
+	for (int i = minRectIdx; i < rect_points.size() - 1; i++) {
+		rect_points[i] = rect_points[i + 1];
+	}
+	rect_points.pop_back();
+
+	float angle1 = GetAngle(Point2f(rect_points[1].x - rect_points[0].x, rect_points[1].y - rect_points[0].y),
+		Point2f(rect_points[2].x - rect_points[0].x, rect_points[2].y - rect_points[0].y));
+	float angle2 = GetAngle(Point2f(rect_points[0].x - rect_points[1].x, rect_points[0].y - rect_points[1].y),
+		Point2f(rect_points[2].x - rect_points[1].x, rect_points[2].y - rect_points[1].y));
+	float angle3 = GetAngle(Point2f(rect_points[0].x - rect_points[2].x, rect_points[0].y - rect_points[2].y),
+		Point2f(rect_points[1].x - rect_points[2].x, rect_points[1].y - rect_points[2].y));
+
+	//cout << angle1 << '\t' << angle2 << '\t' << angle3 << endl;
+
+	mark = -1;
+	if (angle1 > angle2 && angle1 > angle3) {
+		P1[0] = Point2f(rect_points[0].x, rect_points[0].y);
+		mark = 0;
+		//cout << "angle1 " << angle1 << endl;
+	}
+	else if (angle2 > angle1 && angle2 > angle3) {
+		P1[0] = Point2f(rect_points[1].x, rect_points[1].y);
+		mark = 1;
+		//cout << "angle2 " << angle2 << endl;
+	}
+	else if (angle3 > angle1 && angle3 > angle2) {
+		P1[0] = Point2f(rect_points[2].x, rect_points[2].y);
+		mark = 2;
+		//cout << "angle3 " << angle3 << endl;
+	}
+
+	Point2f midPoint = Point2f((rect_points[(mark + 1) % rect_points.size()].x + rect_points[(mark + 2) % rect_points.size()].x) / 2,
+		(rect_points[(mark + 1) % rect_points.size()].y + rect_points[(mark + 2) % rect_points.size()].y) / 2);
+
+	float tempX = rect_points[(mark + 1) % rect_points.size()].x - P1[0].x;
+	float tempY = rect_points[(mark + 1) % rect_points.size()].y - P1[0].y;
+	float x1 = (tempX) / sqrt(tempX * tempX + tempY * tempY);
+	float y1 = (tempY) / sqrt(tempX * tempX + tempY * tempY);
+
+	tempX = rect_points[(mark + 2) % rect_points.size()].x - P1[0].x;
+	tempY = rect_points[(mark + 2) % rect_points.size()].y - P1[0].y;
+	float x2 = (tempX) / sqrt(tempX * tempX + tempY * tempY);
+	float y2 = (tempY) / sqrt(tempX * tempX + tempY * tempY);
+
+	if (P1[0].x <= midPoint.x) {
+		if (y1 < y2) {
+			P1[1] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+			P1[2] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+		}
+		else if (y1 > y2) {
+			P1[1] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+			P1[2] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+		}
+		else if (y1 == y2) {
+			if (P1[0].y > midPoint.y) {
+				if (x1 < x2) {
+					P1[1] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+					P1[2] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+				}
+				else {
+					P1[1] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+					P1[2] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+				}
+			}
+		}
+	}
+	else {
+		if (y1 > y2) {
+			P1[1] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+			P1[2] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+		}
+		else if (y1 < y2) {
+			P1[1] = Point2f(rect_points[(mark + 2) % rect_points.size()].x, rect_points[(mark + 2) % rect_points.size()].y);
+			P1[2] = Point2f(rect_points[(mark + 1) % rect_points.size()].x, rect_points[(mark + 1) % rect_points.size()].y);
+		}
+	}
+
+	//for (int i = 0; i < 3; i++) {
+	//	cout << P1[i].x << ',' << P1[i].y << '\t';
+	//}
+	//cout << endl;
+
+	Point2f P2[4]{
+		Point2f(35, 35), Point2f(745, 35), Point2f(35, 745), Point2f(715, 715)
+	};
+
+	Mat dstImg;
+	Mat matrix = getPerspectiveTransform(P1, P2);
+	warpPerspective(midImg, dstImg, matrix, Size(780, 780), 1, 0, Scalar(255, 255, 255));
+	QRcodes.push_back(dstImg);
+	imwrite(".jpg", dstImg);
+	imwrite("./pic3/dstImg" + to_string(QRcodes.size() - 1) + ".png", dstImg);
 }
