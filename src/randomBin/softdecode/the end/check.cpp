b76c233968e6a9d241714f398d939f0090fde3c0
AivyChen <145818007+AivyChen@users.noreply.github.com>
2024-03-27 11:17:45 +0800

    Update check.cpp

--- src/randomBin/softdecode/the end/check.cpp
+++ src/randomBin/softdecode/the end/check.cpp
@@ -1,25 +1,37 @@
 #include "check.h"
 
-void checkVout(const string in_bin_path, const string out_bin_path, const string vout_bin_path)
+void checkVout(const string out_bin_path, const string vout_bin_path)
 {
-	std::ifstream file1(in_bin_path, std::ios::binary);
-	std::ifstream file2(out_bin_path, std::ios::binary);
+	std::ifstream file1(out_bin_path, std::ios::binary);
 	std::ofstream diff_file(vout_bin_path, std::ios::binary);
 
-	if (!file1.is_open() || !file2.is_open() || !diff_file.is_open()) {
+	if (!file1.is_open() || !diff_file.is_open()) {
 		std::cerr << "Error opening files!" << std::endl;
 		return;
 	}
 
-	char byte1, byte2;
-	while (file1.get(byte1) && file2.get(byte2))
+	char byte1;
+	while (file1.get(byte1))
 	{
-		uchar byte3 = ~((uchar)byte1 ^ (uchar)byte2);
-		diff_file.put(byte3);
+		char byte[9];
+		for (int i = 0; i < 9; i++)
+		{
+			file1.get(byte[i]);
+			byte1 |= byte[i];
+		}
+		if (byte1 == 0)
+		{
+			for(int i=0;i<10;i++)
+				diff_file.put(0);
+		}
+		else
+		{
+			for (int i = 0; i < 10; i++)
+				diff_file.put(255);
+		}
 	}
 
 	file1.close();
-	file2.close();
 	diff_file.close();
 
 	return;
