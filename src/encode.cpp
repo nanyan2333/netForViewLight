0d0d199c5ab8590383f213eb07e3f5e340466554
Lee Mark <117705652+MRORANGE11112@users.noreply.github.com>
2024-04-06 00:03:29 +0800

    Update encode.cpp

--- src/encode.cpp
+++ src/encode.cpp
@@ -19,7 +19,7 @@ void ReadBinFile(string& str, const string fileName) {
 int en = 0;
 
 void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
-	int frame_rate = 10;
+	int frame_rate = 20;
 	double target_duration_seconds = stoi(limitTime) * 1.0 / 1000;
 	// 计算最大帧数（向上取整）  
 	int target_frames = static_cast<int>(std::ceil(target_duration_seconds * frame_rate));
@@ -29,25 +29,25 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 	for (int i = 0; i < data1.size(); i++)
 		data.push_back(data1[i]);
 	while (true) {
-		Mat src(780, 780, CV_8UC3, Scalar(255, 255, 255));
+		Mat src(1440, 1440, CV_8UC3, Scalar(255, 255, 255));
 
 		//左上定位点
 		rectangle(src, Point(0, 0), Point(69, 69), Scalar(0, 0, 0), -1);
 		rectangle(src, Point(10, 10), Point(59, 59), Scalar(255, 255, 255), -1);
 		rectangle(src, Point(20, 20), Point(49, 49), Scalar(0, 0, 0), -1);
 		//右上定位点
-		rectangle(src, Point(710, 0), Point(779, 69), Scalar(0, 0, 0), -1);
-		rectangle(src, Point(720, 10), Point(769, 59), Scalar(255, 255, 255), -1);
-		rectangle(src, Point(730, 20), Point(759, 49), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(1370, 0), Point(1439, 69), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(1380, 10), Point(1429, 59), Scalar(255, 255, 255), -1);
+		rectangle(src, Point(1390, 20), Point(1419, 49), Scalar(0, 0, 0), -1);
 		//左下定位点
-		rectangle(src, Point(0, 710), Point(69, 779), Scalar(0, 0, 0), -1);
-		rectangle(src, Point(10, 720), Point(59, 769), Scalar(255, 255, 255), -1);
-		rectangle(src, Point(20, 730), Point(49, 759), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(0, 1370), Point(69, 1439), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(10, 1380), Point(59, 1429), Scalar(255, 255, 255), -1);
+		rectangle(src, Point(20, 1390), Point(49, 1419), Scalar(0, 0, 0), -1);
 
 		//右下辅助点
-		rectangle(src, Point(690, 690), Point(739, 739), Scalar(0, 0, 0), -1);
-		rectangle(src, Point(700, 700), Point(729, 729), Scalar(255, 255, 255), -1);
-		rectangle(src, Point(710, 710), Point(719, 719), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(1350, 1350), Point(1399, 1399), Scalar(0, 0, 0), -1);
+		rectangle(src, Point(1360, 1360), Point(1389, 1389), Scalar(255, 255, 255), -1);
+		rectangle(src, Point(1370, 1370), Point(1379, 1379), Scalar(0, 0, 0), -1);
 
 		int bits = 8;
 		int flag = 0;
@@ -57,8 +57,8 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 		while (true) {
 			real_page++;
 			page = real_page;
-			for (int col = 0; col < 78; col++) {
-				for (int row = 0; row < 78; row++) {
+			for (int col = 0; col < 144; col++) {
+				for (int row = 0; row < 144; row++) {
 					if (data.size() == key)
 					{
 						flag++;
@@ -66,8 +66,8 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 						data.push_back('\0');
 					}
 					if (flag == 2) {
-						Mat backGround(860, 860, CV_8UC3, Scalar(255, 255, 255));
-						Rect area(40, 40, 780, 780);
+						Mat backGround(1600, 1600, CV_8UC3, Scalar(255, 255, 255));
+						Rect area(40, 40, 1440, 1440);
 						src.copyTo(backGround(area));
 						//imwrite(to_string(en++) + ".png", backGround);
 						num++;
@@ -78,9 +78,11 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 						img.push_back(backGround);
 						return;
 					}
-					if ((col < 8 && (row < 8 || row > 69)) || (col > 69 && row < 8) || (col > 67 && col < 75 && row>67 && row < 75)||(col==77&&row>66)) {
+					if ((col < 8 && (row < 8 || row > 135)) || (col > 135 && row < 8) || (col > 123 && col < 141 && row>123 && row < 141)) {
 						continue;
 					}
+					else if (col == 143 && row > 56)
+						continue;
 					else if (col == 0 && row < 16) {
 						if (page % 2 == 1) {
 							rectangle(src, Point(col * 10, row * 10), Point(col * 10 + 9, row * 10 + 9), Scalar(0, 0, 0), -1);
@@ -108,8 +110,8 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 				}
 			}
 			//imwrite("1.png", src);
-			Mat backGround(860, 860, CV_8UC3, Scalar(255, 255, 255));
-			Rect area(40, 40, 780, 780);
+			Mat backGround(1600, 1600, CV_8UC3, Scalar(255, 255, 255));
+			Rect area(80, 80, 1440, 1440);
 			src.copyTo(backGround(area));
 			//imwrite(to_string(en++) + ".png", backGround);
 			num++;
@@ -129,10 +131,10 @@ void DrawQRCode(string data1, vector<Mat>& img, const string limitTime) {
 }
 
 void PicToVideo(vector<Mat>& img, const string fileName) {
-	int frame_rate = 10;
-	VideoWriter video(fileName, VideoWriter::fourcc('m', 'p', '4', 'v'), frame_rate, Size(860, 860));
+	int frame_rate = 20;
+	VideoWriter video(fileName, VideoWriter::fourcc('m', 'p', '4', 'v'), frame_rate, Size(1600, 1600));
 
-	for (int i = 0; i < img.size(); i++){
+	for (int i = 0; i < img.size(); i++) {
 		Mat image = img[i].clone();
 		video << image;
 	}
