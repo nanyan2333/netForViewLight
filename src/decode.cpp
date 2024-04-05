a338de67be8be58a3b26c0500bd4873bf16edd2a
Lee Mark <117705652+MRORANGE11112@users.noreply.github.com>
2024-04-05 18:04:07 +0800

    Update decode.cpp

--- src/decode.cpp
+++ src/decode.cpp
@@ -1,11 +1,11 @@
-﻿#include "decode.h"
+#include"decode.h"
 void VideoToPic(const string fileName, vector<Mat>& img) {
 	VideoCapture video(fileName);
 	int frame_num = video.get(CAP_PROP_FRAME_COUNT);
 	double fps = video.get(CAP_PROP_FPS);
 	cout << fps << endl;
 
-	int rate = 1;
+	int rate = 3;
 
 	for (int i = 1; i <= frame_num; i++) {
 		Mat frame;
@@ -24,12 +24,13 @@ void QRCodeToData(vector<Mat>& img, string& data) {
 	int page[1000] = { 0 };
 	int tmp = 0;
 	int flag = 0;
+	string ta[1000];
 	for (int i = 0; i < img.size(); i++) {
 		for (int col = 0; col < 78; col++) {
 			for (int row = 0; row < 78; row++) {
 				if (col == 0 && row == 16)
 				{
-					if (page[tmp] != 0)
+					if (page[tmp] != 0||tmp==0)
 					{
 						flag = 1;
 						tmp = 0;
@@ -38,11 +39,10 @@ void QRCodeToData(vector<Mat>& img, string& data) {
 					}
 					else
 						page[tmp]++;
-					tmp = 0;
 					count1 = 0;
 				}
 				if ((col < 8 && (row < 8 || row > 69)) || (col > 69 && row < 8)
-					|| (col > 67 && col < 75 && row>67 && row < 75)) {
+					|| (col > 67 && col < 75 && row>67 && row < 75)||(col==77&&row>66) ){
 					continue;
 				}
 				else if (col == 0 && row >= 8 && row < 16) {
@@ -53,13 +53,13 @@ void QRCodeToData(vector<Mat>& img, string& data) {
 				}
 				else {
 					if (count == 0)
-						data += '\0';
+						ta[tmp] += '\0';
 					Scalar color = img[i].at<uchar>(Point(col * 10 + 5, row * 10 + 5));
 					if (color[0] > 128) {
-						data[key] += '\0';
+						ta[tmp][key] += '\0';
 					}
 					else {
-						data[key] += 1 * pow(2, count);
+						ta[tmp][key] += 1 * pow(2, count);
 					}
 					count++;
 					if (count == 8) {
@@ -74,10 +74,22 @@ void QRCodeToData(vector<Mat>& img, string& data) {
 				break;
 			}
 		}
+		tmp = 0;
+		key = 0;
 	}
-	//int pos = data.rfind("genshin");
-	//data.erase(data.begin()+pos, data.end());
-	//cout << data << endl;
+	for (int i = 1; i < img.size(); i++)
+	{
+		if (ta[i].empty())
+			for (int j = 0; j < 728; j++)
+			{
+				if (j % 13 == 0&&j!=0)
+					data += 1;
+				else
+					data += '\0';
+			}
+		else
+			data += ta[i];
+	}
 }
 void WriteBinFile(string& str, const string fileName) {
 	ofstream outFile;
